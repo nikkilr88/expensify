b01fe14491a00049a9a0e841d54c2802ecf57afc
Nicole <nicole.sattler@live.com>
2019-03-02 13:16:06 -0600

    Update entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/playground/redux-expensify.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
