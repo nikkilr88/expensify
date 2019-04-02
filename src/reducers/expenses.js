b6807d1a99b51bf568e77f3b35c637bab1b6a1e4
Nicole <nicole.sattler@live.com>
2019-04-01 19:35:05 -0600

    Add expenses reducer

---
+++ src/reducers/expenses.js
@@ -0,0 +1,23 @@
+const expensesState = []
+
+const expensesReducer = (state = expensesState, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense]
+    case 'REMOVE_EXPENSE':
+      return state.filter(exp => exp.id !== action.id)
+    case 'EDIT_EXPENSE':
+      return state.map(expense => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        }
+      })
+    default:
+      return state
+  }
+}
+
+export default expensesReducer
