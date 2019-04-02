8aa3251809135d74a4ce87ac0454526a97669bdd
Nicole <nicole.sattler@live.com>
2019-04-01 19:33:48 -0600

    Import store config, actions and selectors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import configureStore from './store/configureStore'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
+
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 import AppRouter from './routers/AppRouter'
+
+const store = configureStore()
+
+store.dispatch(setTextFilter('water'))
+
+store.dispatch(
+  addExpense({ description: 'Water Bill', amount: 14000, createdAt: -1000 })
+)
+store.dispatch(
+  addExpense({ description: 'Electric Bill', amount: 12000, createdAt: -1000 })
+)
+
+const state = store.getState()
+
+console.log(getVisibleExpenses(state.expenses, state.filters))
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'))
