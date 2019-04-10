464093fb0cb6d8e0a3be01fed63208361813a38c
Nicole <nicole.sattler@live.com>
2019-04-10 14:53:47 -0500

    Set up provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
@@ -13,17 +13,19 @@ import AppRouter from './routers/AppRouter'
 
 const store = configureStore()
 
-store.dispatch(setTextFilter('water'))
+// store.dispatch(setTextFilter('water'))
 
-store.dispatch(
-  addExpense({ description: 'Water Bill', amount: 14000, createdAt: -1000 })
-)
-store.dispatch(
-  addExpense({ description: 'Electric Bill', amount: 12000, createdAt: -1000 })
-)
+store.dispatch(addExpense({ description: 'Water Bill', amount: 14000, createdAt: -1000 }))
+store.dispatch(addExpense({ description: 'Electric Bill', amount: 12000, createdAt: -1000 }))
 
 const state = store.getState()
 
 console.log(getVisibleExpenses(state.expenses, state.filters))
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'))
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('root'))
