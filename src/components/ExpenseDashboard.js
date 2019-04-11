17860dd4e2c7ec1758d47442034a7378ca79f545
Nicole <nicole.sattler@live.com>
2019-04-11 14:53:13 -0500

    Render expense list filters component

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import ExpenseList from './ExpenseList'
+import ExpenseListFilters from './ExpenseListFilters'
 
 const ExpenseDashboard = () => (
   <div>
     <p>this is the dashboard</p>
+    <ExpenseListFilters />
     <ExpenseList />
   </div>
 )
