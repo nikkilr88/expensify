7e7568846dbdd9c74f68d14cd64eca65f4455814
Nicole <nicole.sattler@live.com>
2019-04-11 14:53:53 -0500

    Change expense layout, add remove button

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { removeExpense } from '../actions/expenses'
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
   <div>
+    <h4>{description}</h4>
     <p>
-      {amount} - {description} (Date:{createdAt})
+      {amount} - {createdAt}
     </p>
+    <button
+      onClick={() => {
+        dispatch(removeExpense({ id }))
+      }}
+    >
+      Remove
+    </button>
   </div>
 )
 
-export default ExpenseListItem
+export default connect()(ExpenseListItem)
