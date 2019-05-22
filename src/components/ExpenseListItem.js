ab40b92cf725b7af578dba937553641af9cad75c
Nicole <nicole.sattler@live.com>
2019-05-22 11:58:52 -0500

    Add link to edit, take out remove button

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,21 +1,15 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { removeExpense } from '../actions/expenses'
+import { Link } from 'react-router-dom'
 
-const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => (
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
-    <h4>{description}</h4>
+    <h4>
+      <Link to={`/edit/${id}`}>{description}</Link>
+    </h4>
     <p>
       {amount} - {createdAt}
     </p>
-    <button
-      onClick={() => {
-        dispatch(removeExpense({ id }))
-      }}
-    >
-      Remove
-    </button>
   </div>
 )
 
-export default connect()(ExpenseListItem)
+export default ExpenseListItem
