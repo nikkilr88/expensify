049a8684d1ed3421dc0a1ecf0b0c10931e6fa611
Nicole <nicole.sattler@live.com>
2019-05-22 11:58:08 -0500

    Add form and remove button

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import ExpenseForm from './ExpenseForm'
+import { editExpense, removeExpense } from '../actions/expenses'
 
 const EditExpense = props => {
-  return <p>Edit expense {props.match.params.id}</p>
+  return (
+    <div>
+      <p>Edit expense {props.match.params.id}</p>
+
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={expense => {
+          props.dispatch(editExpense(props.expense.id, expense))
+          props.history.push('/dashboard')
+        }}
+      />
+
+      <button
+        onClick={() => {
+          props.dispatch(removeExpense({ id: props.expense.id }))
+          props.history.push('/dashboard')
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  )
 }
 
-export default EditExpense
+const mapStateToProps = (state, props) => ({
+  expense: state.expenses.find(expense => expense.id === props.match.params.id)
+})
+
+export default connect(mapStateToProps)(EditExpense)
