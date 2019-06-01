1f24815c49a83c1a9885680d79833e07581c8151
Nicole <nicole.sattler@live.com>
2019-06-01 11:12:27 -0500

    Add test data

---
+++ src/tests/fixtures/expenses.js
@@ -0,0 +1,29 @@
+import moment from 'moment'
+
+export default [
+  {
+    id: '1',
+    description: 'rent',
+    amount: 150000,
+    note: '',
+    createdAt: moment(0)
+  },
+  {
+    id: '2',
+    description: 'tea',
+    amount: 1000,
+    note: '',
+    createdAt: moment(0)
+      .subtract(4, 'days')
+      .valueOf()
+  },
+  {
+    id: '3',
+    description: 'course',
+    amount: 13500,
+    note: 'udemy',
+    createdAt: moment(0)
+      .add(4, 'days')
+      .valueOf()
+  }
+]
