2a004809d827d9945d6fc356f177c23eb1ec4ed9
Nicole <nicole.sattler@live.com>
2019-06-04 17:52:24 -0500

    Import React

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => (
   <header>
     <h1>Expensify App</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
+    <NavLink to='/' activeClassName='is-active' exact={true}>
       Home
     </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
+    <NavLink to='/create' activeClassName='is-active'>
       Create
     </NavLink>
-    <NavLink to="/dashboard" activeClassName="is-active">
+    <NavLink to='/dashboard' activeClassName='is-active'>
       Dashboard
     </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
+    <NavLink to='/help' activeClassName='is-active'>
       Help
     </NavLink>
   </header>
