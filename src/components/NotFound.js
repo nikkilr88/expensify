01103c59d11ffe24c0d2c36cd0fc4cfa0596f83e
Nicole <nicole.sattler@live.com>
2019-06-21 11:25:48 -0500

    Import React

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,9 @@
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NotFound = () => (
   <div>
-    404! - <Link to="/">Go Back</Link>
+    404! - <Link to='/'>Go Back</Link>
   </div>
 )
 
