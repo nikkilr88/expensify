56e432c16f7fe82599d62ceaa77198cf596ddb37
Nicole <nicole.sattler@live.com>
2019-07-02 10:55:30 -0500

    Import react-dates

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
+import AppRouter from './routers/AppRouter'
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-
-import AppRouter from './routers/AppRouter'
+import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 
