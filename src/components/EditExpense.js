4ca78263b61077707683057e99c41e1a14fbb45a
Nicole <nicole.sattler@live.com>
2019-07-03 12:13:00 -0500

    Refactor to class component

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -3,33 +3,51 @@ import { connect } from 'react-redux'
 import ExpenseForm from './ExpenseForm'
 import { editExpense, removeExpense } from '../actions/expenses'
 
-const EditExpense = props => {
-  return (
-    <div>
-      <p>Edit expense {props.match.params.id}</p>
+export class EditExpense extends Component {
+  onSubmit = (id, expense) => {
+    this.props.editExpense(id, expense)
+    this.props.history.push('/dashboard')
+  }
 
-      <ExpenseForm
-        expense={props.expense}
-        onSubmit={expense => {
-          props.dispatch(editExpense(props.expense.id, expense))
-          props.history.push('/dashboard')
-        }}
-      />
+  onClick = id => {
+    this.props.removeExpense(id)
+    this.props.history.push('/dashboard')
+  }
 
-      <button
-        onClick={() => {
-          props.dispatch(removeExpense({ id: props.expense.id }))
-          props.history.push('/dashboard')
-        }}
-      >
-        Remove
-      </button>
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <p>Edit expense {this.props.match.params.id}</p>
+
+        <ExpenseForm
+          expense={this.props.expense}
+          onSubmit={expense => {
+            this.onSubmit(this.props.expense.id, expense)
+          }}
+        />
+
+        <button
+          onClick={() => {
+            this.onClick({ id: this.props.expense.id })
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    )
+  }
 }
+
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  removeExpense: id => dispatch(removeExpense(id))
+})
 
 const mapStateToProps = (state, props) => ({
   expense: state.expenses.find(expense => expense.id === props.match.params.id)
 })
 
-export default connect(mapStateToProps)(EditExpense)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EditExpense)
