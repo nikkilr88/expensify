68fe0fb976c9e09c255ee9e2eb6abdff483381de
Nicole <nicole.sattler@live.com>
2019-07-17 16:47:46 -0500

    Render ExpensesSummary

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import ExpenseList from './ExpenseList'
 import ExpenseListFilters from './ExpenseListFilters'
+import ExpensesSummary from './ExpensesSummary'
 
 const ExpenseDashboard = () => (
   <div>
     <p>this is the dashboard</p>
     <ExpenseListFilters />
+    <ExpensesSummary />
     <ExpenseList />
   </div>
 )
