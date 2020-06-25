b44c65acae9caa1e894e0b3129775ef99d5529a2
Nicole Sattler <nicole.sattler@live.com>
2020-06-25 17:42:39 -0500

    Start login page styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,15 +4,18 @@ import { startLogin } from '../actions/auth'
 
 export const LoginPage = ({ startLogin }) => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+      <div className="box-layout__box">
+        <h1 className="box-layout__title">Expensify App</h1>
+        <p>Get your expenses under control</p>
+        <button onClick={startLogin}>Login</button>
+      </div>
     </div>
   )
 }
 
 const mapDispatchToProps = dispatch => ({
-  startLogin: () => dispatch(startLogin())
+  startLogin: () => dispatch(startLogin()),
 })
 
 export default connect(undefined, mapDispatchToProps)(LoginPage)
